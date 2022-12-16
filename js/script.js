33d47a4327be233e430275118d3c1a2e87d27bf0
Divyajyoti <divyajyoti182000@gmail.com>
2022-12-16 17:09:27 +0530

    40% toggle UI well javascript logic completed

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+let display = document.querySelector('.calculator__screen--text');
+// console.log(display.textContent);
+
+//Operations
+
+const plus = document.querySelector('.calculator__keypad--key-7');
+const subtract = document.querySelector('.calculator__keypad--key-11');
+const multiplication = document.querySelector('.calculator__keypad--key-16');
+const division = document.querySelector('.calculator__keypad--key-15');
+const result = document.querySelector('.calculator__keypad--key-18');
+
+let sum_result = 0;
+let subtract_result = 0;
+let multiply_result = 1;
+let division_result = 1;
+
